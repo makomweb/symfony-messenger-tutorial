87cf7d408f27690f623ef45e5e3ee203a82829ee
Martin Komischke <martin.komischke@gmail.com>
2021-06-15 08:56:02 +0200

    more preparation

---
+++ src/Message/AttendConfirmationEmail.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+
+class AttendConfirmationEmail
+{
+    private $id;
+
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId() : int
+    {
+        return $this->id;
+    }
+}
---
+++ src/MessageHandler/AttendConfirmationEmailHandler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\MessageHandler;
+
+
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class AttendConfirmationEmailHandler implements MessageHandlerInterface
+{
+
+}
