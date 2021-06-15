ca42664bfcf36b8473e1a10d2c7acaeb2040e104
Martin Komischke <martin.komischke@gmail.com>
2021-06-15 11:29:46 +0200

    inject repo into the message handler

--- src/MessageHandler/OrderPlacedMessageHandler.php
+++ src/MessageHandler/OrderPlacedMessageHandler.php
@@ -4,10 +4,21 @@ namespace App\MessageHandler;
 
 
 use App\Message\OrderPlacedMessage;
+use App\Repository\OrderRepository;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class OrderPlacedMessageHandler implements MessageHandlerInterface
 {
+    /**
+     * @var OrderRepository
+     */
+    private $repository;
+
+    public function __construct(OrderRepository $repository)
+    {
+        $this->repository = $repository;    
+    }
+    
     public function __invoke(OrderPlacedMessage $message)
     {
         echo 'Order placed';
