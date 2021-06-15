b0cc92b3d73a9aca1cf8121159941d479ee613f7
Martin Komischke <martin.komischke@gmail.com>
2021-06-15 09:58:06 +0200

    setup the order endpoint

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -7,9 +7,15 @@ use App\OrderConfirmationEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
+    /**
+     * @Route("/order", name="order")
+     * @param MessageBusInterface $bus
+     * @return Response
+     */
     public function placeOrder(MessageBusInterface $bus)
     {
         $bus->dispatch(new OrderConfirmationEmail(1));
