17b7e274c1ea1fbe420cf4b42eb2ade028c4d128
Martin Komischke <martin.komischke@outlook.com>
2021-06-17 11:07:44 +0200

    redirect to upload

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->redirectToRoute('upload');
+    }
+}
