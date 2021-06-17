97f91a983c73b81cbc0d1b52d6270c6be4c8d9ff
Martin Komischke <martin.komischke@outlook.com>
2021-06-17 07:52:01 +0200

    start with uploading files

---
+++ src/Controller/FileUploadController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FileUploadController extends AbstractController
+{
+    /**
+     * @Route("/file/upload", name="file_upload")
+     */
+    public function index(): Response
+    {
+        return $this->render('file_upload/index.html.twig', [
+            'controller_name' => 'FileUploadController',
+        ]);
+    }
+}
---
+++ src/Entity/FileUpload.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FileUploadRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FileUploadRepository::class)
+ */
+class FileUpload
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->Status;
+    }
+
+    public function setStatus(string $Status): self
+    {
+        $this->Status = $Status;
+
+        return $this;
+    }
+}
