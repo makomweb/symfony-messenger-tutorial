e2009171c7a0f5547da463d63697d4bd31779157
Martin Komischke <martin.komischke@outlook.com>
2021-06-17 10:29:04 +0200

    handle resizing

--- src/MessageHandler/FileUploadMessageHandler.php
+++ src/MessageHandler/FileUploadMessageHandler.php
@@ -6,31 +6,40 @@ namespace App\MessageHandler;
 use App\Message\FileUploadMessage;
 use App\Repository\FileUploadRepository;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use App\Service\ImageResizer;
 
 class FileUploadMessageHandler implements MessageHandlerInterface
 {
     /**
      * @var FileUploadRepository
+     * @var ImageResizer
      */
     private $repository;
 
-    public function __construct(FileUploadRepository $repository)
+    public function __construct(string $uploadDir,
+        FileUploadRepository $repository,
+        ImageResizer $resizer)
     {
-        $this->repository = $repository;    
+        $this->repository = $repository;
+        $this->resizer = $resizer;
+        $this->uploadDir = $uploadDir;
     }
     
     public function __invoke(FileUploadMessage $message)
     {
         echo 'File upload received: ' . $message->getId();
 
-        // Fake being busy.
-        sleep(1);
-
         // fetch entity
         $entity = $this->repository->find($message->getId());
 
         if ($entity)
         {
+            $path = $this->uploadDir . '/' . $entity->getName();
+
+            echo "Start resizing for: " . $path;
+
+            $this->resizer->resize($path);
+
             // update status
             $entity->setStatus("handled");
             $this->repository->save($entity);
---
+++ src/Service/ImageResizer.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service;
+
+use Imagine\Gd\Imagine;
+use Imagine\Image\Box;
+
+class ImageResizer
+{
+    private const MAX_WIDTH = 200;
+    private const MAX_HEIGHT = 150;
+
+    private $imagine;
+
+    public function __construct()
+    {
+        $this->imagine = new Imagine();
+    }
+
+    public function resize(string $filename): void
+    {
+        list($iwidth, $iheight) = getimagesize($filename);
+        $ratio = $iwidth / $iheight;
+        $width = self::MAX_WIDTH;
+        $height = self::MAX_HEIGHT;
+        if ($width / $height > $ratio) {
+            $width = $height * $ratio;
+        } else {
+            $height = $width / $ratio;
+        }
+
+        $photo = $this->imagine->open($filename);
+
+        $thumbnail = $this->thumbNailPath($filename);
+
+        $photo->resize(new Box($width, $height))->save($thumbnail);
+    }
+
+    private function thumbNailPath(string $filename): string
+    {
+        $path_parts = pathinfo($filename);
+
+        return $path_parts['dirname'] . '/' . $path_parts['filename'] . '-thumbnail.' . $path_parts['extension'];
+    }
+}
